f6939b48ac29c06e888cbbe4a73183a91940d9e6
Gustas <gustass1@gmail.com>
2021-05-17 02:55:55 +0300

    B-2: added user mangoose schema file and service for getting user data

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './user.schema';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+	constructor(
+		@InjectModel(User.name) private readonly userModel: Model<UserDocument>
+	) {}
+
+	async findByUsername(username: string): Promise<User> {
+		return this.userModel.find({ username }).exec()[0];
+	}
+}

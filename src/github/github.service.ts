7090181f48f47a1b4bdbd58cdd08ff21cde459e2
Gustas <gustass1@gmail.com>
2021-05-31 03:06:36 +0300

    B-3: data count per page moved to constants

--- src/github/github.service.ts
+++ src/github/github.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { IRepository } from './helpers/GithubData';
+import {
+	COMMENT_COUNT_PER_PULL_REQUEST,
+	PULL_REQUEST_COUNT_PER_REPOSITORY
+} from './helpers/GithubParameters';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const fetch = require('node-fetch');
@@ -30,16 +34,19 @@ export class GithubService {
 	}
 
 	async getRepositoryPullRequests(url: string) {
-		const response = await fetch(`${url}?state=all&per_page=5`, {
-			method: 'GET',
-			headers: {
-				'Content-Type': 'application/json',
-				Accept: 'application/json',
-				Authorization: `Bearer ${this.configService.get<string>(
-					'GITHUB_TOKEN'
-				)}`
+		const response = await fetch(
+			`${url}?state=all&per_page=${PULL_REQUEST_COUNT_PER_REPOSITORY}`,
+			{
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json',
+					Accept: 'application/json',
+					Authorization: `Bearer ${this.configService.get<string>(
+						'GITHUB_TOKEN'
+					)}`
+				}
 			}
-		});
+		);
 
 		const data = await response.json();
 
@@ -47,16 +54,19 @@ export class GithubService {
 	}
 
 	async getPullRequestComments(url: string) {
-		const response = await fetch(`${url}?per_page=5`, {
-			method: 'GET',
-			headers: {
-				'Content-Type': 'application/json',
-				Accept: 'application/json',
-				Authorization: `Bearer ${this.configService.get<string>(
-					'GITHUB_TOKEN'
-				)}`
+		const response = await fetch(
+			`${url}?per_page=${COMMENT_COUNT_PER_PULL_REQUEST}`,
+			{
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json',
+					Accept: 'application/json',
+					Authorization: `Bearer ${this.configService.get<string>(
+						'GITHUB_TOKEN'
+					)}`
+				}
 			}
-		});
+		);
 
 		const data = await response.json();
 
